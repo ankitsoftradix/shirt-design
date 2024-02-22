de40487104b3d732a08ec0db7da69588bd8ae458
Ankit <ankit@softradix.com>
2024-02-22 17:16:16 +0530

    basic box added

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,19 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import styles from "./Modal.module.scss";
 
 const Modal = () => {
-  return <div className={styles.modalWrap}>Modal</div>;
+  return (
+    <div className={styles.modalWrap}>
+      <Canvas camera={{ position: [2, 2, 2] }}>
+        <mesh>
+          <boxGeometry />
+          <meshNormalMaterial />
+        </mesh>
+        <OrbitControls makeDefault />
+      </Canvas>
+    </div>
+  );
 };
 
 export default Modal;
