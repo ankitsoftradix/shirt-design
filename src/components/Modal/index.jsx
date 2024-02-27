a7180719e89c7f2ed33a28a75e92d0e84055b55d
Ankit <ankit@softradix.com>
2024-02-27 16:53:14 +0530

    dog animation added

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,44 +1,15 @@
-import { Canvas, useFrame } from "@react-three/fiber";
-import styles from "./Modal.module.scss";
 import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
-import { DoubleSide } from "three";
-
-const ModalLayout = () => {
-  const cubeRef = useRef();
-  useFrame((state) => {
-    cubeRef.current.rotation.y += 0.01;
-    cubeRef.current.position.y =
-      Math.sin(2 * state.clock.elapsedTime - Math.PI) + 1.5;
-  });
-  return (
-    <>
-      <mesh ref={cubeRef}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-      <mesh
-        position={[0, 0, 0]}
-        rotation={[Math.PI / 2, 0, 0]}
-        scale={[1, 1, 1]}
-      >
-        <planeGeometry args={[4, 4]} />
-        <meshBasicMaterial side={DoubleSide} />
-      </mesh>
-    </>
-  );
-};
+import { Canvas } from "@react-three/fiber";
+import ModelSkeleton from "../ModalSkeleton";
+import styles from "./Modal.module.scss";
 
 const Modal = () => {
   return (
     <div className={styles.modalWrap}>
-      <Canvas
-        camera={{
-          position: [3, 3, 3],
-        }}
-      >
-        <ModalLayout />
+      <Canvas camera={{ position: [0, 1, 4] }}>
+        <ModelSkeleton />
         <OrbitControls />
+        <gridHelper />
       </Canvas>
     </div>
   );
---
+++ src/components/ModalSkeleton/index.jsx
@@ -0,0 +1,47 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { Suspense, useEffect, useRef } from "react";
+
+const ModelSkeleton = (props) => {
+  const group = useRef();
+  const { nodes, animations } = useGLTF("./modal/dog.glb");
+  const { actions } = useAnimations(animations, group);
+  useEffect(() => {
+    actions.Embarrassed.play();
+  }, []);
+  return (
+    <Suspense>
+      <ambientLight intensity={4} />
+      <group ref={group} {...props} dispose={null}>
+        <group>
+          <group name="Geometry">
+            <group name="Rover_Props">
+              <skinnedMesh
+                name="Rover_travel_bag"
+                geometry={nodes.Rover_travel_bag.geometry}
+                material={nodes.Rover_travel_bag.material}
+                skeleton={nodes.Rover_travel_bag.skeleton}
+              />
+              {/* <skinnedMesh
+                name="Rover_footprint"
+                geometry={nodes.Rover_footprint.geometry}
+                material={nodes.Rover_footprint.material}
+                skeleton={nodes.Rover_footprint.skeleton}
+              /> */}
+            </group>
+            <skinnedMesh
+              name="Rover"
+              geometry={nodes.Rover.geometry}
+              material={nodes.Rover.material}
+              skeleton={nodes.Rover.skeleton}
+            />
+          </group>
+          <primitive object={nodes.Root} />
+        </group>
+      </group>
+    </Suspense>
+  );
+};
+
+useGLTF.preload("./modal/dog.glb");
+
+export default ModelSkeleton;
